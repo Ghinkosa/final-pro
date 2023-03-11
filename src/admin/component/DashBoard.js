ea2ad18b989e29d34aa7ce7394b007572fa84fef
Galata Hinkosa <galata17137@gmail.com>
2023-03-11 14:39:46 +0300

    third commit from me

--- src/admin/component/DashBoard.js
+++ src/admin/component/DashBoard.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { FaUserGraduate } from "react-icons/fa";
-
+import { FaUserGraduate,FaChalkboardTeacher,FaBookReader,FaBookOpen,FaUserTie,FaTrash} from "react-icons/fa";
+import {MdOutlineRestore} from "react-icons/md"
 export const DashBoard = () => {
   return (
     <div className="mt-4">
-      <div className="fs-2 ms-2 text-dark ">DashBoard</div>
+      <div className="fs-2 ms-2 text-white ">DashBoard</div>
       <div className="d-flex flex-wrap">
         <div className="card box-sizes d-flex flex-row">
           <div className="my-auto ps-4">
@@ -19,40 +19,85 @@ export const DashBoard = () => {
         </div>
         <div className="card box-sizes d-flex flex-row">
           <div className="my-auto ps-4">
-            <FaUserGraduate size={40} color="#ec5782" />
+            <FaChalkboardTeacher size={40} color="#294BF9" />
           </div>
           <div className="card-body">
-            <div className="card-title m-auto ms-3 fs-5 dashboard-color">
-              Student
+            <div className="card-title m-auto ms-3 fs-5 dashboard-color-t">
+              Teacher
             </div>
-            <div className="card-subtitle ms-3 fs-3 dashboard-color">3345</div>
+            <div className="card-subtitle ms-3 fs-3 dashboard-color-t">345</div>
           </div>
         </div>
         <div className="card box-sizes d-flex flex-row">
           <div className="my-auto ps-4">
-            <FaUserGraduate size={40} color="#ec5782" />
+            <FaBookReader size={40} color="#554D00" />
           </div>
           <div className="card-body">
-            <div className="card-title m-auto ms-3 fs-5 dashboard-color">
-              Student
+            <div className="card-title m-auto ms-3 fs-5 dashboard-color-c">
+               Class and Section
             </div>
-            <div className="card-subtitle ms-3 fs-3 dashboard-color">3345</div>
+            <div className="card-subtitle ms-3 fs-3 dashboard-color-c">45</div>
           </div>
         </div>
         <div className="card box-sizes d-flex flex-row">
           <div className="my-auto ps-4">
-            <FaUserGraduate size={40} color="#ec5782" />
+            <FaBookOpen size={40} color="#3300FF" />
           </div>
           <div className="card-body">
-            <div className="card-title m-auto ms-3 fs-5 dashboard-color">
-              Student
+            <div className="card-title m-auto ms-3 fs-5 dashboard-color-s">
+              Subject
             </div>
-            <div className="card-subtitle ms-3 fs-3 dashboard-color">3345</div>
+            <div className="card-subtitle ms-3 fs-3 dashboard-color-s">45</div>
           </div>
         </div>
       </div>
       <div className="ms-5 mt-5 stud-list">
-        <div className="p-4 fs-4">Student List</div>
+        <div className="p-4 fs-4 text-dark ">Student List</div>
+        <div>
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">name</th>
+      <th scope="col">Gender</th>
+      <th scope="col">phone</th>
+      <th scope="col">DOB</th>
+      <th scope="col">photo</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark Otto</td>
+      <td>M</td>
+      <td>0945235563</td>
+      <td>july 30 2000</td>
+      <td><FaUserTie size={40} color="#ec5782"/></td>
+      <td><FaTrash size={25} color="red"/><MdOutlineRestore size={30} color="#20D7D7" className="ms-3"/></td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob Thornton</td>
+      <td>F</td>
+      <td>0945235563</td>
+      <td>july 30 2000</td>
+      
+      <td><FaUserTie size={40} color="#ec5782"/></td>
+      <td><FaTrash size={25} color="red"/><MdOutlineRestore size={30} color="#20D7D7" className="ms-3"/></td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry the Bird</td>
+      <td>M</td>
+      <td>0945235563</td>
+      <td>july 30 2000</td>
+      <td><FaUserTie size={40} color="#294BF9"/></td>
+      <td><FaTrash size={25} color="red"/><MdOutlineRestore size={30} color="#20D7D7" className="ms-3"/></td>
+    </tr>
+  </tbody>
+</table>
+        </div>
       </div>
     </div>
   );
--- src/admin/component/SideMenu.js
+++ src/admin/component/SideMenu.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import photo from "./images/photo1.jpg";
 import {
   FaHome,
@@ -9,6 +9,11 @@ import {
   FaRegHandPaper,
 } from "react-icons/fa";
 export default function SideMenu() {
+
+  const [active,setActive]=useState(1);
+  const activator=(a)=>{
+    setActive(a);
+  }
   return (
     <div className="sticky-top position-absolute">
       <div className="container-fluid">
@@ -39,66 +44,146 @@ export default function SideMenu() {
               </div>
             </div>
             <ul className="all-list">
-              <li className="list">
+              {
+                active==1 ? (<li className="list">
+                <a className="nav-link">
+                  <i className="pb-2">
+                    <FaHome size={30} color="#ff6b00" />
+                  </i>
+                  <span className=" ms-3 d-none d-sm-inline fs-3 Actives">
+                    Dashbord
+                  </span>
+                </a>
+              </li>):
+              <li className="list" onClick={()=>activator(1)}>
                 <a className="nav-link">
                   <i className="">
-                    <FaHome size={30} color="black" />
+                    <FaHome size={30} />
                   </i>
-                  <span className=" ms-3 d-none d-sm-inline text-dark fs-3 ">
+                  <span className=" ms-3 d-none d-sm-inline fs-3 ">
                     Dashbord
                   </span>
                 </a>
               </li>
-              <li className="list">
+                  }
+              {
+                active==2 ?( <li className="list">
                 <a className="nav-link">
                   <i className="">
-                    <FaRegPaperPlane size={30} color="black" />
+                    <FaRegPaperPlane size={30} color="#ff6b00"/>
                   </i>
-                  <span className="ms-3 d-none d-sm-inline text-dark fs-3">
+                  <span className="ms-3 d-none d-sm-inline fs-3 Actives">
+                    Message
+                  </span>
+                </a>
+              </li>):
+              <li className="list" onClick={()=>activator(2)}>
+                <a className="nav-link">
+                  <i className="">
+                    <FaRegPaperPlane size={30} />
+                  </i>
+                  <span className="ms-3 d-none d-sm-inline fs-3">
                     Message
                   </span>
                 </a>
               </li>
-              <li className="list">
+             }
+             {
+              active==3 ?(   <li className="list" >
+              <a className="nav-link ">
+                <i className="">
+                  <FaUser size={30} color="#ff6b00"/>
+                </i>
+                <span className="ms-3 d-none d-sm-inline fs-3 Actives">
+                  Users
+                </span>
+              </a>
+            </li>):
+              <li className="list" onClick={()=>activator(3)}>
                 <a className="nav-link ">
                   <i className="">
-                    <FaUser size={30} color="black" />
+                    <FaUser size={30} />
                   </i>
-                  <span className="ms-3 d-none d-sm-inline text-dark fs-3">
+                  <span className="ms-3 d-none d-sm-inline fs-3">
                     Users
                   </span>
                 </a>
               </li>
-              <li className="list">
+             } 
+            {
+            active==4 ?(  <li className="list">
+              <a className="nav-link">
+                <i className="">
+                  <FaBook size={30} color="#ff6b00" />
+                </i>
+                <span className="ms-3 d-none d-sm-inline fs-3 Actives">
+                   Marks
+                </span>
+              </a>
+
+            </li>):
+              <li className="list" onClick={()=>activator(4)}>
                 <a className="nav-link">
                   <i className="">
-                    <FaBook size={30} color="black" />
+                    <FaBook size={30}  />
                   </i>
-                  <span className="ms-3 d-none d-sm-inline text-dark fs-3">
+                  <span className="ms-3 d-none d-sm-inline fs-3">
                     Marks
                   </span>
                 </a>
               </li>
+                }
+
+              {active==5 ?(
+                              
               <li className="list">
+              <a className="nav-link">
+                <i className="">
+                  <FaRegHandPaper size={30} color="#ff6b00"  />
+                </i>
+                <span className="ms-3 d-none d-sm-inline  fs-3 Actives">
+                  Attendance
+                </span>
+              </a>
+            </li>
+              ):
+              
+              
+              <li className="list" onClick={()=>activator(5)}>
                 <a className="nav-link">
                   <i className="">
-                    <FaRegHandPaper size={30} color="black" />
+                    <FaRegHandPaper size={30}  />
                   </i>
-                  <span className="ms-3 d-none d-md-inline text-dark fs-3">
+                  <span className="ms-3 d-none d-sm-inline  fs-3 ">
                     Attendance
                   </span>
                 </a>
               </li>
-              <li className="list">
+}
+{
+  active==6 ?(
+    <li className="list">
+    <a className="nav-link ">
+      <i className="d-sm-inline">
+        <FaRegCalendarAlt size={30} color="#ff6b00" />
+      </i>
+      <span className="ms-3 d-none d-sm-inline  fs-3 Actives">
+        Schedule
+      </span>
+    </a>
+  </li>
+  ):
+              <li className="list" onClick={()=>activator(6)}>
                 <a className="nav-link ">
                   <i className="d-sm-inline">
-                    <FaRegCalendarAlt size={30} color="black" />
+                    <FaRegCalendarAlt size={30} />
                   </i>
-                  <span className="ms-3 d-none d-sm-inline text-dark fs-3">
+                  <span className="ms-3 d-none d-sm-inline  fs-3">
                     Schedule
                   </span>
                 </a>
               </li>
+}
             </ul>
           </div>
         </div>
