ebb36395fd45c3b8dab16e194fd6af383e798548
Galata Hinkosa <galata17137@gmail.com>
2023-04-27 11:36:19 +0300

    by galataeh

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Login from "./Login";
 import {Routes, Route,useNavigate} from "react-router-dom";
 import NotFound from "./NotFound";
 import { useEffect } from "react";
-import Students from "./admin/Students";
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
       <Routes>
          <Route path="/" element={<Login />}/>
          <Route path="/dashboard" element={<Admin />}/>
-         <Route path="/das" element={<Students />}/>
          <Route path="*" element={< NotFound/>}/>
       </Routes>
       
